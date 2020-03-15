f757f23d35329ea3488430b6012a3cf34335d5a6
Valery Gurov <va1ery@ya.ru>
2020-03-15 21:08:13 +0300

    Добавил БД и предсталения

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+/*         public string Index()
+        {
+            return "This is my default action...";
+        } */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+/*         public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        } */
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
